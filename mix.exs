aaf3c66c96016561140db307962036d95cd682d8
Miron Brezuleanu <mbrezu@gmail.com>
2026-07-12 12:57:35 +0300

    fixup :-|

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HtmlAsCode.MixProject do
   def project do
     [
       app: :html_as_code,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
