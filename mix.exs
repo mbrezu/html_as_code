4af9e48e27a9befa38deb48cdab63c7b090ae7ac
Miron Brezuleanu <mbrezu@gmail.com>
2026-07-19 21:10:43 +0300

    more tags

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HtmlAsCode.MixProject do
   def project do
     [
       app: :html_as_code,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
